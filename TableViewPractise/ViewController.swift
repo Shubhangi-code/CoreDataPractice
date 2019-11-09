b8f6f88f1dce32d7811c2f1e852f39b4f882f5d6
Dominator <amitsamant@outlook.com>
2019-11-09 14:29:33 +0530

    Initial Commit

---
+++ TableViewPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractise
+//
+//  Created by dominator on 09/11/19.
+//  Copyright © 2019 dominator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
